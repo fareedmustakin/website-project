0dc5748deb3e023fadf0e926d41edc2ffe0f97d9
Fareed Mustakin <fareed@nimbusforwork.com>
2025-02-10 03:18:30 +0800

    Updated homepage title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <Navbar />
       <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">Testing Git Commit Updates 🚀</h1>
+        <h1 className="text-4xl font-bold text-blue-600">Testing Automatic Deployments 🚀</h1>
         <p className="text-lg text-gray-500">Product Manager | UX Designer</p>
       </main>
     </div>
